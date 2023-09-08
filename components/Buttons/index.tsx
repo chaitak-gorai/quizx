fa71b452dc1be90655e09c92813f02a9e286a761
chaitak-gorai <chaitakgorai@gmail.com>
2023-09-09 00:22:03 +0530

    Handle functions updated

--- components/Buttons/index.tsx
+++ components/Buttons/index.tsx
@@ -3,26 +3,16 @@ import React from "react";
 import { useQuizConfig } from "@/store";
 import axios from "axios";
 import { nanoid } from "nanoid";
+import toast from "react-hot-toast";
 
 export default function Button() {
   const changeStatus = useQuizConfig((state:any) => state.changeStatus);
   const addUserId = useQuizConfig((state:any) => state.addUserId);
   const addQuizId = useQuizConfig((state:any) => state.addQuizId);
  const config = useQuizConfig((state:any) => state.config);
-     const addQuizPartner = useQuizConfig((state: any) => state.addQuizPartner)
-  async function sentUserId() {
-    const userId= nanoid()
-    console.log(userId,'usreid')
-    addUserId(userId)
-    var quizId = config.quizId;
-    if(quizId===''){
-     quizId=`quiz--${userId}`
-      console.log(quizId)
-      await addQuizId(quizId)
-         
-    }else{
-      addQuizPartner(quizId.split('--')[1])
-    }
+  const addQuizPartner = useQuizConfig((state: any) => state.addQuizPartner)
+
+  async function sentUserId(userId:string,quizId:string) {
     const data = await axios.post('/api/joined', {
         userId: userId,
         quizId: quizId,
@@ -30,21 +20,43 @@ export default function Button() {
       console.log(data)
 
   }
-const handleStart = () => {
+const handleJoin = () => {
+
+ 
+  var quizId = config.quizId
+  if (quizId === '') {
+    const toastId=toast.error('Please enter quizId')
+  } else {
+      changeStatus('start')
+       const userId = nanoid()
+       console.log(userId, 'usreid')
+       addUserId(userId)
+    addQuizPartner(quizId.split('--')[1])
+    sentUserId(userId, quizId)
+  }
+  
+}
+const handleCreate = () => {
   changeStatus("start")
-  sentUserId()
+    const userId = nanoid()
+    console.log(userId, 'usreid')
+    addUserId(userId)
+  const quizId = `quiz--${userId}`
+  addQuizId(quizId)
+  sentUserId(userId, quizId)
 }
   return (
     <>
       <button
-        onClick={handleStart}
+        onClick={handleJoin}
         type='button'
         className='  m-auto  text-lg font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 py-4 px-10 w-1/2  '
       >
         Join Quiz
       </button>
       <button
-        onClick={handleStart}
+     
+        onClick={handleCreate}
         type='button'
         className='  m-auto  text-lg font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 py-4 px-10 w-1/2  '
       >
